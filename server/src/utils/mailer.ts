f7d94f6ec0556ae37593073739d219e0ddbf83b6
Taras Demianiuk <demtaras1@gamil.com>
2026-06-12 20:29:30 +0300

    added some changes for bugs

--- server/src/utils/mailer.ts
+++ server/src/utils/mailer.ts
@@ -42,8 +42,8 @@ const translations = {
 
 export const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
