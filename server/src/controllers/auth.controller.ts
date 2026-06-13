079ef832feac85f2d50e7d955a01bd032b83a5e0
Taras Demianiuk <demtaras1@gamil.com>
2026-06-13 20:47:46 +0300

    added changes (cookie)

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -13,8 +13,8 @@ import { getIo } from '../socket/socketInstance';
 const COOKIE_OPTIONS = {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'none',
+  secure: true,
 } as const;
 
 async function register(req: Request, res: Response) {
